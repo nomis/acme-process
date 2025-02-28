593fc7cd5819e1c6ca5692e8442c1d3a18453d8c
Simon Arlott <sa.me.uk>
2025-02-28 18:37:30 +0000

    Use renewal info

--- acme-process.py
+++ acme-process.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright 2016-2017,2020  Simon Arlott
+# Copyright 2016-2017,2020,2025  Simon Arlott
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,10 +14,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import datetime
 import configparser
 import fcntl
 import hashlib
 import os
+import random
 import re
 import subprocess
 import sys
@@ -33,24 +35,43 @@ etcdir = os.path.join(home, "etc")
 
 
 def cert_process(name, cfg):
+	ari = ""
+
 	if os.path.exists(cfg["req"]):
 		request = False
 
 		configured = cert_get_configured_names(name, cfg)
 
 		if os.path.exists(cfg["cert"]):
-			with subprocess.Popen(["openssl", "x509", "-noout", "-checkend", str(86400 * cfg.getint("days")), "-in", cfg["cert"]],
-					stdin=subprocess.DEVNULL,
-					stdout=subprocess.DEVNULL,
-					stderr=subprocess.DEVNULL) as proc:
-				retcode = proc.wait(timeout=30)
-				if retcode != 0:
+			(ari, start, end) = cert_renewalinfo(name, cfg)
+
+			if ari is not None and start is not None and end is not None:
+				now = datetime.datetime.now(tz=datetime.timezone.utc)
+				renew_at = start + datetime.timedelta(seconds=random.randrange((end - start).total_seconds()))
+				next_run = now + datetime.timedelta(hours=cfg.getint("hours"))
+
+				if now >= renew_at or end < next_run:
 					syslog.syslog("{0}: will expire soon".format(name))
 					request = True
+			else:
+				ari = ""
+
+				with subprocess.Popen(["openssl", "x509", "-noout", "-checkend", str(86400 * cfg.getint("days")), "-in", cfg["cert"]],
+						stdin=subprocess.DEVNULL,
+						stdout=subprocess.DEVNULL,
+						stderr=subprocess.DEVNULL) as proc:
+					retcode = proc.wait(timeout=30)
+					if retcode != 0:
+						syslog.syslog("{0}: will expire soon".format(name))
+						request = True
 
 			existing = cert_get_existing_names(name, cfg)
 			if configured - existing:
 				syslog.syslog("{0}: has new names: {1}".format(name, configured - existing))
+
+				if not configured & existing:
+					ari = ""
+
 				request = True
 		elif not os.path.exists(os.path.dirname(cfg["cert"])):
 			syslog.syslog("{0}: cert directory does not exist".format(name))
@@ -69,7 +90,7 @@ def cert_process(name, cfg):
 
 		if request:
 			syslog.syslog("{0}: requesting certificate".format(name))
-			if cert_request(name, cfg):
+			if cert_request(name, cfg, ari):
 				syslog.syslog("{0}: certificate updated".format(name))
 
 
@@ -135,7 +156,46 @@ def cert_get_configured_names(name, cfg):
 	return set(config.sections())
 
 
-def cert_request(name, cfg):
+def cert_renewalinfo(name, cfg):
+	retcode = 0
+	with subprocess.Popen(["/usr/local/lib/acme-tiny/acme_tiny.py",
+				"--quiet",
+				"--directory", cfg["directory"],
+				"renewalinfo",
+				"--account-key", cfg["account_key"],
+				"--cert", cfg["cert"],
+			],
+			stdin=subprocess.DEVNULL,
+			stdout=subprocess.PIPE,
+			stderr=subprocess.PIPE,
+			universal_newlines=True) as proc:
+		(stdout, stderr) = proc.communicate(timeout=600)
+		retcode = proc.wait(timeout=30)
+
+	if retcode == 0:
+		ari = None
+		start = None
+		end = None
+		for line in stdout.splitlines():
+			if line.startswith("OK: "):
+				(ari, start, end) = line.split(" ")[1:4]
+				if start.endswith("Z"):
+					start = datetime.datetime.fromisoformat(start[:-1]).replace(tzinfo=datetime.timezone.utc)
+				else:
+					start = None
+				if end.endswith("Z"):
+					end = datetime.datetime.fromisoformat(end[:-1]).replace(tzinfo=datetime.timezone.utc)
+				else:
+					end = None
+
+		syslog.syslog("{0}: renewal info: {1} {2} {3}".format(name, ari, str(start), str(end)))
+		return (ari, start, end)
+	else:
+		print("Error processing {0!r}, acme-tiny returned {1}\n{2}\n{3}".format(name, retcode, stdout, stderr))
+		return (None, None, None)
+
+
+def cert_request(name, cfg, ari=""):
 	syslog.syslog("{0}...".format(name))
 
 	try:
@@ -161,6 +221,7 @@ def cert_request(name, cfg):
 				"--config", cfg["config"],
 				"--req", cfg["req"],
 				"--path", "C = US, O = Internet Security Research Group, CN = ISRG Root X1"
+				"--ari", ari,
 			],
 			stdin=subprocess.DEVNULL,
 			stdout=subprocess.PIPE,
